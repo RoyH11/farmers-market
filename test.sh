2a11cd25bcab1c44f3ec74ce784170e567a6c5b8
RoyH11 <ruoqiuhuang@gmail.com>
2025-07-30 11:24:00 -0500

    remove old compiled files before rerun

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Compile and run tests
+rm -f bin/*Test.class
 javac -cp "lib/*:bin" -d bin test/*.java src/*.java
 java -cp "lib/*:bin" org.junit.platform.console.ConsoleLauncher execute --scan-classpath
